0d0d65ed986a89f4cf3e0a01f031d937afcc5b9a
FilipD <dziubekfilip@gmail.com>
2025-12-18 15:08:09 +0100

    feat: add automatic metro.config.js generation to expo plugin

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -3,6 +3,7 @@ import { createRunOncePlugin } from '@expo/config-plugins';
 import { withAndroidMaven } from './withAndroidMaven';
 import { withIosPodfileSource } from './withIosPodfileSource';
 import { withIosPrivacyInfo } from './withIosPrivacyInfo';
+import { withMetroConfig } from './withMetroConfig';
 
 const pkg = require('../../package.json');
 
@@ -11,13 +12,13 @@ const pkg = require('../../package.json');
  * - Android: Adds Verisoul Maven repository to build.gradle
  * - iOS: Adds Verisoul iOS SDK source to Podfile
  * - iOS: Configures privacy manifest for Device ID collection
+ * - Metro: Creates metro.config.js with automatic SDK resolution
  */
 const withVerisoul: ConfigPlugin = (config) => {
   config = withAndroidMaven(config);
-
   config = withIosPodfileSource(config);
-
   config = withIosPrivacyInfo(config);
+  config = withMetroConfig(config);
 
   return config;
 };
